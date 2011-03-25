e698a8a63923d6998d1c162f7b982b8952ff8882
Max Cantor <mxcantor@gmail.com>
2011-03-24 21:29:32 -0700

    custom eq class instance for Edit Costs

--- Text/EditDistance/EditCosts.hs
+++ Text/EditDistance/EditCosts.hs
@@ -5,11 +5,17 @@ module Text.EditDistance.EditCosts where
 data EditCosts = EditCosts {
     deletionCost :: !Int,
     insertionCost :: !Int,
-    substitutionCost :: !(Either !Int (Char -> Char -> Int)),
+    substitutionCost :: !(Either Int (Char -> Char -> Int)),
     transpositionCost :: !Int
   }
-  deriving (Eq)
 
+instance Eq EditCosts where
+  (==) a@EditCosts{substitutionCost = Left as} b@EditCosts{substitutionCost = Left bs} = 
+    (as == bs) && 
+    (deletionCost a == deletionCost b) && 
+    (insertionCost a == insertionCost b) &&
+    (transpositionCost a == transpositionCost b)  
+  (==) _ _  = False
 
 defaultEditCosts :: EditCosts
 defaultEditCosts = EditCosts {
